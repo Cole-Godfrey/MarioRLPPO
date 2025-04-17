f8db18b44f94558d83994d474c506594751f2f9c
Cole-Godfrey <colegodfrey34@gmail.com>
2025-04-16 20:59:31 -0700

    Revised train.py and test.py

--- test.py
+++ test.py
@@ -3,33 +3,8 @@ from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import GrayScaleObservation
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-from gym import Wrapper
-import os
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import BaseCallback
-
-# fix for seed and options being passed when env.reset didn't accept them
-class SeedFixWrapper(Wrapper):
-    def reset(self, **kwargs):
-        kwargs.pop('seed', None)
-        kwargs.pop('options', None)
-        return self.env.reset()
-
-class TrainAndLoggingCallback(BaseCallback):
-    def __init__(self, check_freq, save_path, verbose=1):
-        super(TrainAndLoggingCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.save_path = save_path
-
-    def _init_callback(self):
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
-
-    def _on_step(self):
-        if self.n_calls % self.check_freq == 0:
-            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
-            self.model.save(model_path)
-        return True
+from train import SeedFixWrapper
 
 env = gym_super_mario_bros.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode='human')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
@@ -44,4 +19,3 @@ state = env.reset()
 while True:
     action, _ = model.predict(state)
     state, reward, done, info = env.step(action)
-
--- train.py
+++ train.py
@@ -46,4 +46,3 @@ callback = TrainAndLoggingCallback(check_freq=1000000, save_path=CHECKPOINT_DIR)
 model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, n_steps=512)
 
 model.learn(total_timesteps=1000000, callback=callback)
-
